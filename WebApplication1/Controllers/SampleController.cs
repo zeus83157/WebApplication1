9986df737ab33d40e2a0994d4b0cd39455e56a51
Kernel Yang <zeus83157@gmail.com>
2023-08-07 21:20:17 +0800

    加入專案檔案。

---
+++ WebApplication1/Controllers/SampleController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class SampleController : Controller
+    {
+        // GET: Sample
+        public ActionResult MainPageA()
+        {
+            return View();
+        }
+
+        public ActionResult MainPageB()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/HTMLHelperExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+
+namespace WebApplication1.Models
+{
+    public static class HTMLHelperExtension
+    {
+        public static MvcHtmlString Script(this HtmlHelper htmlHelper, string key, Func<object, HelperResult> template)
+        {
+            htmlHelper.ViewContext.HttpContext.Items[key] = template;
+            return MvcHtmlString.Empty;
+        }
+
+        public static IHtmlString Script(this HtmlHelper htmlHelper, string key)
+        {
+            var template = htmlHelper.ViewContext.HttpContext.Items[key] as Func<object, HelperResult>;
+            if (template != null)
+            {
+                htmlHelper.ViewContext.Writer.Write(template(null));
+            }
+            return MvcHtmlString.Empty;
+        }
+    }
+}
